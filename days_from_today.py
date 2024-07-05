ea28dccf02da0230c7137273920be55ad1f7b032
Volodymyr Vladykin <vova@pocketguard.com>
2024-07-05 22:42:47 +0300

    Added get_days_from_today() function

---
+++ days_from_today.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+# Expects date in 'YYYY-MM-DD' format.
+# Returns diff in days between date and today.
+# In case of date in future, returns negative number.
+def get_days_from_today(date: str) -> int:
+    raw_date_pattern = "%Y-%m-%d"
+
+    parsed_date = datetime.strptime(date, raw_date_pattern)
+
+    today_date = datetime.today()
+
+    # return days between two dates
+    return today_date.toordinal() - parsed_date.toordinal()
+
+
+
+raw_input = input("Enter date in 'YYYY-MM-DD' format: ")
+try:
+    days_between = get_days_from_today(raw_input)
+    print(f"days between are {days_between}")
+except ValueError:
+    print(f"Invalid input \'{raw_input}\'. Make sure you use 'YYYY-MM-DD' format")
